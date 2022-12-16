a3bcb4476c8f57c4ee894167e90068dc31a024bf
Dmitriy Dezhin <dadezhin@edu.hse.ru>
2022-12-16 18:30:41 +0300

    aboba

--- main.cpp
+++ main.cpp
@@ -75,26 +75,169 @@ const ll INF = INT64_MAX;
 #define repb2(I, N) for (ll I = (N); I >= 1; --I)
 #define FOR(IT, ST, N) for (ll IT = (ST); (IT) <= (N); ++(IT))
 #define FORB(IT, N, ST) for (ll IT = (N); (IT) >= (ST); --(IT))
-template<class T,class T2>int cmin(T &a,T2 b){return a>b?a=b,1:0;}
-template<class T,class T2>int cmax(T &a,T2 b){return a<b?a=b,1:0;}
-template<class T>T sqr(T a){return a*a;}
-template<class T,class T2>T mmin(T a,T2 b){return a<b?a:b;}
-template<class T,class T2>T mmax(T a,T2 b){return a>b?a:b;}
-template<class T,class ...T2>T mmin(T a,T2 ...b){return mmin(a,mmin(b...));}
-template<class T,class ...T2>T mmax(T a,T2 ...b){return mmax(a,mmax(b...));}
+
+template <typename T> T mod_inv_in_range(T a, T m) {
+
+    T x = a, y = m;
+    T vx = 1, vy = 0;
+    while (x) {
+        T k = y / x;
+        y %= x;
+        vy -= k * vx;
+        std::swap(x, y);
+        std::swap(vx, vy);
+    }
+    return vy < 0 ? m + vy : vy;
+}
+
+template <typename T> T mod_inv(T a, T m) {
+    a %= m;
+    a = a < 0 ? a + m : a;
+    return mod_inv_in_range(a, m);
+}
+
+template <int MOD_> struct Mint {
+    static constexpr int MOD = MOD_;
+
+private:
+    int v;
+
+public:
+
+    Mint() : v(0) {}
+    Mint(int64_t v_) : v(int(v_ % MOD)) { if (v < 0) v += MOD; }
+    explicit operator int() const { return v; }
+    friend std::ostream& operator << (std::ostream& out, const Mint& n) { return out << int(n); }
+    friend std::istream& operator >> (std::istream& in, Mint& n) { int64_t v_; in >> v_; n = Mint(v_); return in; }
+
+    friend bool operator == (const Mint& a, const Mint& b) { return a.v == b.v; }
+    friend bool operator != (const Mint& a, const Mint& b) { return a.v != b.v; }
+
+    Mint inv() const {
+        Mint res;
+        res.v = mod_inv_in_range(v, MOD);
+        return res;
+    }
+    friend Mint inv(const Mint& m) { return m.inv(); }
+    Mint neg() const {
+        Mint res;
+        res.v = v ? MOD-v : 0;
+        return res;
+    }
+    friend Mint neg(const Mint& m) { return m.neg(); }
+
+    Mint operator- () const {
+        return neg();
+    }
+    Mint operator+ () const {
+        return Mint(*this);
+    }
+
+    Mint& operator ++ () {
+        v ++;
+        if (v == MOD) v = 0;
+        return *this;
+    }
+    Mint& operator -- () {
+        if (v == 0) v = MOD;
+        v --;
+        return *this;
+    }
+    Mint& operator += (const Mint& o) {
+        v -= MOD-o.v;
+        v = (v < 0) ? v + MOD : v;
+        return *this;
+    }
+    Mint& operator -= (const Mint& o) {
+        v -= o.v;
+        v = (v < 0) ? v + MOD : v;
+        return *this;
+    }
+    Mint& operator *= (const Mint& o) {
+        v = int(int64_t(v) * int64_t(o.v) % MOD);
+        return *this;
+    }
+    Mint& operator /= (const Mint& o) {
+        return *this *= o.inv();
+    }
+
+    friend Mint operator ++ (Mint& a, int) { Mint r = a; ++a; return r; }
+    friend Mint operator -- (Mint& a, int) { Mint r = a; --a; return r; }
+    friend Mint operator + (const Mint& a, const Mint& b) { return Mint(a) += b; }
+    friend Mint operator - (const Mint& a, const Mint& b) { return Mint(a) -= b; }
+    friend Mint operator * (const Mint& a, const Mint& b) { return Mint(a) *= b; }
+    friend Mint operator / (const Mint& a, const Mint& b) { return Mint(a) /= b; }
+};
+
+template <typename T> T pow(T a, ll b) {
+    T r = 1; while (b) { if (b & 1) r *= a; b >>= 1; a *= a; } return r;
+}
+
+using mint = Mint<998244353>;
+
+vector<mint> fact(1, 1);
+vector<mint> inv_fact(1, 1);
+
+void init_facts(size_t len){
+    fact.assign(len + 1, 1);
+    inv_fact.assign(len + 1, 1);
+    rep2(i, len) fact[i] = fact[i - 1] * mint(i);
+    inv_fact[len] = inv(fact[len]);
+    repb2(i, len) inv_fact[i - 1] = inv_fact[i] * mint(i);
+}
+
+mint C(ll n, ll k) {
+    if (k < 0 || k > n) {
+        return 0;
+    }
+    return fact[n] * inv_fact[k] * inv_fact[n - k];
+}
 
 void solve(){
-
+    ll n; cin >> n;
+    graph ogr(n, vec(n, -1));
+    rep(i, n){
+        FOR(j, i, n - 1){
+            cin >> ogr[i][j];
+        }
+    }
+    vector<vector<vector<mint>>> dp(n, vector<vector<mint>>(n, vector<mint>(2, 0)));
+    //vector<vector<mint>> dp(n, vector<mint>(n, 0));
+    rep(i, n) {
+        if (ogr[i][i] == 2) {
+            cout << 0 << el;
+            return;
+        }
+        dp[i][i][0] = 2;
+    }
+    FOR(len, 2, n){
+        rep(l, n - len + 1){
+            ll r = l + len - 1;
+            if (ogr[l][r] == 0) {
+                dp[l][r][0] = dp[l][r - 1][0];
+                dp[l][r][1] = dp[l][r - 1][0] + dp[l][r - 1][1] * 2;
+            }
+            else if (ogr[l][r] == 1){
+                dp[l][r][0] = dp[l][r - 1][0];
+                dp[l][r][1] = 0;
+            } else {
+                dp[l][r][0] = 0;
+                dp[l][r][1] = dp[l][r - 1][0] +  dp[l][r - 1][1] * 2;
+            }
+        }
+    }
+    cout << dp[0][n - 1][0] + dp[0][n - 1][1] << el;
+//    cout << dp[0][n - 1][0] << ' ' << dp[0][n - 1][1] << el;
 }
 
 signed main() {
     FF;
-    ll nt;
-    cin >> nt;
-    while(nt--) {
-        solve();
-    }
-//    solve();
+//    ll nt;
+//    cin >> nt;
+//    while(nt--) {
+//        solve();
+//    }
+    solve();
 
     return 0;
 }
@@ -102,9 +245,16 @@ signed main() {
 // if you have problems, just read the stuff at the bottom
 
 /*
-8 1
-1 2 0 3 1 2 0 3
-1 8
+4
+1 2 0 0
+1 1 1
+1 1
+1
+
+3
+1 2 0
+1 1
+1
 */
 
 /* for inputs
