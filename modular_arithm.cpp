c8796d08e41eab38da843412a117f4fffcb8a391
4svOn <mityusha.dezhin@mail.ru>
2025-09-30 22:04:47 +0300

    aboba

--- modular_arithm.cpp
+++ modular_arithm.cpp
@@ -20,94 +20,94 @@ template <typename T> T mod_inv(T a, T m) {
 
 int MOD = MD_7;
 
-struct Mint {
+struct mint {
 private:
     int v;
 
 public:
 
-    Mint() : v(0) {}
-    Mint(int v_) : v(v_ % MOD) { if (v < 0) v += MOD; }
-    Mint(int64_t v_) : v(int(v_ % MOD)) { if (v < 0) v += MOD; }
+    mint() : v(0) {}
+    mint(int v_) : v(v_ % MOD) { if (v < 0) v += MOD; }
+    mint(int64_t v_) : v(int(v_ % MOD)) { if (v < 0) v += MOD; }
     explicit operator int() const { return v; }
-    friend std::ostream& operator << (std::ostream& out, const Mint& n) { return out << int(n); }
-    friend std::istream& operator >> (std::istream& in, Mint& n) { int64_t v_; in >> v_; n = Mint(v_); return in; }
+    friend std::ostream& operator << (std::ostream& out, const mint& n) { return out << int(n); }
+    friend std::istream& operator >> (std::istream& in, mint& n) { int64_t v_; in >> v_; n = mint(v_); return in; }
 
-    friend bool operator == (const Mint& a, const Mint& b) { return a.v == b.v; }
-    friend bool operator != (const Mint& a, const Mint& b) { return a.v != b.v; }
+    friend bool operator == (const mint& a, const mint& b) { return a.v == b.v; }
+    friend bool operator != (const mint& a, const mint& b) { return a.v != b.v; }
 
-    Mint inv() const {
-        Mint res;
+    mint inv() const {
+        mint res;
         res.v = mod_inv_in_range(v, MOD);
         return res;
     }
-    friend Mint inv(const Mint& m) { return m.inv(); }
-    Mint neg() const {
-        Mint res;
+    friend mint inv(const mint& m) { return m.inv(); }
+    mint neg() const {
+        mint res;
         res.v = v ? MOD-v : 0;
         return res;
     }
-    friend Mint neg(const Mint& m) { return m.neg(); }
+    friend mint neg(const mint& m) { return m.neg(); }
 
-    Mint operator- () const {
+    mint operator- () const {
         return neg();
     }
-    Mint operator+ () const {
-        return Mint(*this);
+    mint operator+ () const {
+        return mint(*this);
     }
 
-    Mint& operator ++ () {
+    mint& operator ++ () {
         v ++;
         if (v == MOD) v = 0;
         return *this;
     }
-    Mint& operator -- () {
+    mint& operator -- () {
         if (v == 0) v = MOD;
         v --;
         return *this;
     }
-    Mint& operator += (const Mint& o) {
+    mint& operator += (const mint& o) {
         v -= MOD-o.v;
         v = (v < 0) ? v + MOD : v;
         return *this;
     }
-    Mint& operator -= (const Mint& o) {
+    mint& operator -= (const mint& o) {
         v -= o.v;
         v = (v < 0) ? v + MOD : v;
         return *this;
     }
-    Mint& operator *= (const Mint& o) {
+    mint& operator *= (const mint& o) {
         v = int(int64_t(v) * int64_t(o.v) % MOD);
         return *this;
     }
-    Mint& operator /= (const Mint& o) {
+    mint& operator /= (const mint& o) {
         return *this *= o.inv();
     }
 
-    friend Mint operator ++ (Mint& a, int) { Mint r = a; ++a; return r; }
-    friend Mint operator -- (Mint& a, int) { Mint r = a; --a; return r; }
-    friend Mint operator + (const Mint& a, const Mint& b) { return Mint(a) += b; }
-    friend Mint operator - (const Mint& a, const Mint& b) { return Mint(a) -= b; }
-    friend Mint operator * (const Mint& a, const Mint& b) { return Mint(a) *= b; }
-    friend Mint operator / (const Mint& a, const Mint& b) { return Mint(a) /= b; }
+    friend mint operator ++ (mint& a, int) { mint r = a; ++a; return r; }
+    friend mint operator -- (mint& a, int) { mint r = a; --a; return r; }
+    friend mint operator + (const mint& a, const mint& b) { return mint(a) += b; }
+    friend mint operator - (const mint& a, const mint& b) { return mint(a) -= b; }
+    friend mint operator * (const mint& a, const mint& b) { return mint(a) *= b; }
+    friend mint operator / (const mint& a, const mint& b) { return mint(a) /= b; }
 };
 
 // template <typename T> T pow(T a, ll b) {
 //     T r = 1; while (b) { if (b & 1) r *= a; b >>= 1; a *= a; } return r;
 // }
 
-vector<Mint> fact(1, 1);
-vector<Mint> inv_fact(1, 1);
+vector<mint> fact(1, 1);
+vector<mint> inv_fact(1, 1);
 
 void init_facts(size_t len){
     fact.assign(len + 1, 1);
     inv_fact.assign(len + 1, 1);
-    rep2(i, len) fact[i] = fact[i - 1] * Mint(i);
+    rep2(i, len) fact[i] = fact[i - 1] * mint(i);
     inv_fact[len] = inv(fact[len]);
-    repb2(i, len) inv_fact[i - 1] = inv_fact[i] * Mint(i);
+    repb2(i, len) inv_fact[i - 1] = inv_fact[i] * mint(i);
 }
 
-Mint C(ll n, ll k) {
+mint C(ll n, ll k) {
     if (k < 0 || k > n) {
         return 0;
     }
